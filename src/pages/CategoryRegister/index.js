33926fa16f8deadd10471806a1c918a8feeee3ae
Leonardo Telles <leo.c.telles@gmail.com>
2020-08-11 12:02:23 -0300

    Update CategoryRegister

--- src/pages/CategoryRegister/index.js
+++ src/pages/CategoryRegister/index.js
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+import api from '../../services/api';
 
 function CategoryRegister() {
+  const history = useHistory();
+
+  const [formData, setFormData] = useState({
+    title: '',
+  });
+
+  function handleInputChange(event) {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const { title } = formData;
+
+    const data = new FormData();
+
+    data.append('title', title);
+
+    await api.post('categories', data);
+
+    alert('Categoria Criada!');
+
+    history.push('/');
+  }
+
   return (
     <>
       <h1>Cadastro de Categorias</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="title">Título</label>
-        <input type="text" id="title" name="title"></input>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          onChange={handleInputChange}
+        ></input>
       </form>
     </>
   );
